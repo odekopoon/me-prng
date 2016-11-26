174fe5f5a510e8b141757515aa327fd81049af44
Koji Yusa <odekopoon@gmail.com>
2016-11-27 00:35:54 +0900

    well512: include utility functions

---
+++ src/genrand.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+unsigned int genrand_int32(void);
+unsigned int genrand_int31(void);
+double genrand_real1(void);
+double genrand_real2(void);
+double genrand_real3(void);
--- src/well512.c
+++ src/well512.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
+#include "genrand.c"
 
 /* initialize state to random bits  */
-static unsigned long state[16];
+static unsigned int state[16];
 /* init should also reset this to 0 */
 static unsigned int index = 0;
 /* return 32 bit random number      */
-unsigned long WELLRNG512(void)
+unsigned int genrand_int32(void)
 {
-	unsigned long a, b, c, d;
+	unsigned int a, b, c, d;
 	a = state[index];
 	c = state[(index+13)&15];
 	b = a^c^(a<<16)^(c<<15);
 	c = state[(index+9)&15];
 	c ^= (c>>11);
 	a = state[index] = b^c;
-	d = a^((a<<5)&0xDA442D24UL);
+	d = a^((a<<5)&0xDA442D24U);
 	index = (index + 15)&15;
 	a = state[index];
 	state[index] = a^b^d^(a<<2)^(b<<18)^(c<<28); return state[index];
